efcd5e49bf5ca73d4f55618d8c3b15a0a8890f88
Anthony Sneed <anthony.sneed@hilti.com>
2020-06-25 15:47:12 -0500

    Add XML documentation to Kafka project.

--- src/EventStreamProcessing.Kafka/KafkaEventProcessor.cs
+++ src/EventStreamProcessing.Kafka/KafkaEventProcessor.cs
@@ -4,10 +4,23 @@ using System.Threading.Tasks;
 
 namespace EventStreamProcessing.Kafka
 {
+    /// <summary>
+    /// Kafka event processor.
+    /// </summary>
+    /// <typeparam name="TSourceKey">Source message key type.</typeparam>
+    /// <typeparam name="TSourceValue">Source message value type.</typeparam>
+    /// <typeparam name="TSinkKey">Sink message key type.</typeparam>
+    /// <typeparam name="TSinkValue">Sink message value type.</typeparam>
     public class KafkaEventProcessor<TSourceKey, TSourceValue, TSinkKey, TSinkValue>
         : EventProcessor<Confluent.Kafka.Message<TSourceKey, TSourceValue>,
             Confluent.Kafka.Message<TSinkKey, TSinkValue>>
     {
+        /// <summary>
+        /// Kafka event processor constructor.
+        /// </summary>
+        /// <param name="consumer">Event consumer.</param>
+        /// <param name="producer">Event producer.</param>
+        /// <param name="handlers">Event handlers.</param>
         public KafkaEventProcessor(
             IEventConsumer<Confluent.Kafka.Message<TSourceKey, TSourceValue>> consumer,
             IEventProducer<Confluent.Kafka.Message<TSinkKey, TSinkValue>> producer, 
@@ -16,6 +29,11 @@ namespace EventStreamProcessing.Kafka
         {
         }
 
+        /// <summary>
+        /// Process event.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Task which will complete when Process finishes.</returns>
         public override async Task Process(CancellationToken cancellationToken = default)
         {
             // Build chain of handlers
--- src/EventStreamProcessing.Kafka/KafkaEventProducer.cs
+++ src/EventStreamProcessing.Kafka/KafkaEventProducer.cs
@@ -3,6 +3,11 @@ using Microsoft.Extensions.Logging;
 
 namespace EventStreamProcessing.Kafka
 {
+    /// <summary>
+    /// Kafka event producer.
+    /// </summary>
+    /// <typeparam name="TKey">Message key type.</typeparam>
+    /// <typeparam name="TValue">Message value type.</typeparam>
     public class KafkaEventProducer<TKey, TValue>
         : IEventProducer<Confluent.Kafka.Message<TKey, TValue>>
     {
@@ -10,6 +15,12 @@ namespace EventStreamProcessing.Kafka
         private readonly string topic;
         private readonly ILogger logger;
 
+        /// <summary>
+        /// Kafka event producer constructor.
+        /// </summary>
+        /// <param name="producer">Kafka producer.</param>
+        /// <param name="topic">Kafka topic.</param>
+        /// <param name="logger"></param>
         public KafkaEventProducer(Confluent.Kafka.IProducer<TKey, TValue> producer, string topic, ILogger logger = null)
         {
             this.producer = producer;
@@ -17,6 +28,10 @@ namespace EventStreamProcessing.Kafka
             this.logger = logger;
         }
 
+        /// <summary>
+        /// Produce Kafka event.
+        /// </summary>
+        /// <param name="sinkEvent">Kafka sink message.</param>
         public void ProduceEvent(Confluent.Kafka.Message<TKey, TValue> sinkEvent)
         {
             producer.Produce(topic, sinkEvent);
